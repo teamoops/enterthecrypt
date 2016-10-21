98b1b5588b9994761c185e0bd1165384ddaf6df5
nc <chuckiels2011@gmail.com>
2016-10-21 18:01:12 -0400

    added more error checking

--- xor.c
+++ xor.c
@@ -7,9 +7,9 @@
 char *argv0;
 
 int main(int argc, char **argv) {
-    int rflag;
+    int rflag = 0;
     ARGBEGIN {
-        case 'r':
+        case 'r': // repeat
             rflag = 1;
     } ARGEND
 
@@ -19,17 +19,20 @@ int main(int argc, char **argv) {
     }
 
     FILE *f1 = argv[0][0] == '-' ? stdin : fopen(argv[0], "r");
-    if(!f1) {errx(2, "failed to open %s", argv[0]);}
+    if(!f1) errx(2, "failed to open %s", argv[0]);
     FILE *f2 = argv[1][0] == '-' ? stdin : fopen(argv[1], "r");
-    if(!f2) {errx(2, "failed to open %s", argv[1]);}
+    if(!f2) errx(2, "failed to open %s", argv[1]);
+    if(f2 == stdin && rflag) errx(3, "cannot -r when reading from pipe");
 
     while(1) {
         char c1 = fgetc(f1);
         char c2 = fgetc(f2);
         if(c1 == EOF || (!rflag && c2 == EOF))
             break;
-        if(c2 == EOF)
-            fseek(f2, 0, SEEK_SET);
+        if(c2 == EOF) {
+            rewind(f2);
+            c2 = fgetc(f2);
+        }
         putchar(c1 ^ c2);
     }
 
